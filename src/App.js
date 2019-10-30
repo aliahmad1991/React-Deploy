1cee219b911121d2847d32567269e54e0f52d9ea
aliahmad1991 <aliahmadheydarzadeh@gmail.com>
2019-10-30 12:57:07 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+       <HashRouter basename='/'>
+   <div>
+    <ul>
+     <li><Link to="/">Home</Link></li>
+     <li><Link to="/about">About</Link></li>
+    </ul>
+    <hr />
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+   </div>
+  </HashRouter>
+    )
+  }
 }
-
-export default App;
